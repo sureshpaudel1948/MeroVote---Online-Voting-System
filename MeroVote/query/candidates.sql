ccb7facfb28a22dbd5e537cc4be16a78e45f6dc3
Suresh Paudel <sureshpaudel59@gmail.com>
2025-02-19 21:13:14 +0545

    Queries Updated

--- MeroVote/query/candidates.sql
+++ MeroVote/query/candidates.sql
@@ -2,7 +2,9 @@ CREATE TABLE candidates (
     id SERIAL PRIMARY KEY, -- Unique identifier for each candidate
     name VARCHAR(255) NOT NULL, -- Candidate's name
     photo VARCHAR(255), -- Path to the candidate's photo
-    election_id VARCHAR(255) NOT NULL, -- Links the candidate to a specific election
+    election_name VARCHAR(255) NOT NULL, -- Links the candidate to a specific election
+    elect_no INT NOT NULL, -- Links the candidate to an election ID
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp for when the candidate was added
-    CONSTRAINT fk_election FOREIGN KEY (election_id) REFERENCES elections(name) ON DELETE CASCADE
+    CONSTRAINT fk_election_name FOREIGN KEY (election_name) REFERENCES elections(name) ON DELETE CASCADE,
+    CONSTRAINT fk_elect_no FOREIGN KEY (elect_no) REFERENCES elections(id) ON DELETE CASCADE
 );
